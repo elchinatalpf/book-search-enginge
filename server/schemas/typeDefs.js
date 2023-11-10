a6f1533b044f041fd10dbea93b2fc03bbd9efdd1
Javier <elchinatalpf@github.com>
2023-11-09 20:09:07 -0600

    Added queries in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,31 +3,45 @@ type Query {
   me: User
 }
 
-  type User {
-    _id: ID
-    username: String
-    email: String
-    bookCount: Int
-    savedBooks: [Book]
-  }
+type User {
+  _id: ID
+  username: String
+  email: String
+  bookCount: Int
+  savedBooks: [Book]
+}
 
-  type Book {
-    _id: ID
-    authors: [String]
-    description: String
-    title: String
-    image: String
-    link: String
-    link: String
-  }
+type Book {
+  _id: ID
+  authors: [String]
+  description: String
+  title: String
+  image: String
+  link: String
+}
 
-  type Mutation {
-    
-  }
+type Auth {
+  token: ID!
+  user: User
+}
 
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(name: String!, email: String!, password: String!): Auth
+  saveBook:(book: SaveBookInput): User
+  removeBook(bookId: String!): User
+}
+
+input BookInput {
+  bookId: String!
+  authors: [String]
+  description: String
+  title: String
+  image: String
+  link: String
+}
 
   
 `;
-
 
 module.exports = typeDefs;
